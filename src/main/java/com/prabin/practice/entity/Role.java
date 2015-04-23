cfb69dc493c6c46608a5654421b064dd1ff70f03
Prabin Amatya <pnamatya@gmail.com>
2015-04-22 19:16:32 -0500

    adding persistence.xml inside meta-inf for jpa diagram editor

---
+++ src/main/java/com/prabin/practice/entity/Role.java
@@ -0,0 +1,9 @@
+package com.prabin.practice.entity;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Role {
+
+}
--- src/main/java/com/prabin/practice/entity/User.java
+++ src/main/java/com/prabin/practice/entity/User.java
@@ -10,6 +10,12 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
 
 	public Integer getId() {
 		return id;
@@ -17,6 +23,30 @@ public class User {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
 	
