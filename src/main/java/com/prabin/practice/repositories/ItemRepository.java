dd2b6586ef14e9a96e6de7d361d5ce224ba5a942
Prabin Amatya <pnamatya@gmail.com>
2015-04-29 14:36:47 -0500

    pageable

--- src/main/java/com/prabin/practice/repositories/ItemRepository.java
+++ src/main/java/com/prabin/practice/repositories/ItemRepository.java
@@ -2,6 +2,7 @@ package com.prabin.practice.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prabin.practice.entity.Blog;
@@ -9,6 +10,6 @@ import com.prabin.practice.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
-	List<Item> findByBlog(Blog blog);
+	List<Item> findByBlog(Blog blog, Pageable page);
 
 }
