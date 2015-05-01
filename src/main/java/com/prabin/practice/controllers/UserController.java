76dafab16c4cde45b5150c78d4ccefc9c18d88af
Prabin Amatya <pnamatya@gmail.com>
2015-04-30 20:02:40 -0500

    register form

--- src/main/java/com/prabin/practice/controllers/UserController.java
+++ src/main/java/com/prabin/practice/controllers/UserController.java
@@ -3,15 +3,22 @@ package com.prabin.practice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.prabin.practice.entity.User;
 import com.prabin.practice.services.UserService;
 
 @Controller
 public class UserController {
 	@Autowired
 	private UserService userService;
+	
+	@ModelAttribute("user")
+	public User construct() {
+		return new User();
+	}
 	
 	@RequestMapping("/users")
 	public String users(Model model) {
